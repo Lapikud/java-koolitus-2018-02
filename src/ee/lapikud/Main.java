99aeb9838cacd7a0a7f94bfaedf520598621d8e2
Arti Zirk <arti.zirk@gmail.com>
2018-02-26 01:26:23 +0200

    First pass of notepad app

--- src/ee/lapikud/Main.java
+++ src/ee/lapikud/Main.java
@@ -1,18 +1,86 @@
 package ee.lapikud;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Hello World");
-        stage.show();
+    private TextArea textArea = new TextArea();
+
+    public void start(Stage primaryStage) {
+        FileChooser fileChooser = new FileChooser();
+        BorderPane root = new BorderPane();
+
+        MenuBar menuBar = new MenuBar();
+        Menu menuFile = new Menu("File");
+        MenuItem open = new MenuItem("Open");
+        open.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if (file != null) {
+                    openFile(file);
+                }
+
+            }
+        });
+        MenuItem save = new MenuItem("Save");
+        save.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                File file = fileChooser.showSaveDialog(primaryStage);
+                if (file != null) {
+                    saveFile(file);
+                }
+            }
+        });
+        menuFile.getItems().addAll(open, save);
+        menuBar.getMenus().add(menuFile);
+        root.setTop(menuBar);
+
+        root.setCenter(textArea);
+
+        Scene scene = new Scene(root,640, 480);
+        primaryStage.setTitle("Notepad");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void openFile(File file) {
+        System.out.printf("Opening file %s\n", file.getAbsolutePath());
+        if (!file.exists()) {
+            return;
+        }
+
+        try {
+            textArea.setText(new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            System.err.printf("Error opening file: %s", e.getMessage());
+        }
+    }
+
+    private void saveFile(File file) {
+        System.out.printf("Saving file %s\n", file.getAbsolutePath());
+        try {
+            Files.write(file.toPath(), textArea.getText().getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            System.err.printf("Error saving file: %s", e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        launch();
+        launch(args);
     }
 }
